300c5aa81749fa759ea6b589b99f6165187bf808
Nirja <nirjapathak3@gmail.com>
2021-06-23 07:56:41 -0700

    Added multiple Get and Calculation functionality.

---
+++ AssignmentCalculatorWebAPI/Controllers/CalculationController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentCalculatorWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class CalculationController : Controller
+    {
+        [HttpGet]
+        [ActionName("Addition")]
+        public double add( double leftNumber, double rightNumber)
+        {
+            return Calculator_ClassLibrary.LibCalculator.add(leftNumber,rightNumber);
+        }
+
+        [HttpGet]
+        [ActionName("Subtraction")]
+        public double sub(double leftNumber, double rightNumber)
+        {
+            return Calculator_ClassLibrary.LibCalculator.sub(leftNumber, rightNumber);
+        }
+
+
+        [HttpGet]
+        [ActionName("Multiplication")]
+        public double mul(double leftNumber, double rightNumber)
+        {
+            return Calculator_ClassLibrary.LibCalculator.mul(leftNumber, rightNumber);
+        }
+
+
+        [HttpGet]
+        [ActionName("Division")]
+        public double div(double leftNumber, double rightNumber)
+        {
+            return Calculator_ClassLibrary.LibCalculator.div(leftNumber, rightNumber);
+        }
+    }
+}
