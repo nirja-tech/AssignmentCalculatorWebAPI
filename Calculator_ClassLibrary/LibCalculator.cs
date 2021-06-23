c5601f4c27a8bf786c58e335898b473ac53024ab
Nirja <nirjapathak3@gmail.com>
2021-06-23 05:40:33 -0700

    Added addition,subtraction,multiplication and division functionality and changed class name.

---
+++ Calculator_ClassLibrary/LibCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Calculator_ClassLibrary
+{
+    public class LibCalculator
+    {
+        static public double add(double leftNumber, double rightNumber)
+        {
+            return leftNumber + rightNumber;
+        }
+
+        static public double sub(double leftNumber, double rightNumber)
+        {
+            return leftNumber - rightNumber;
+        }
+
+        static public double mul(double leftNumber, double rightNumber)
+        {
+            return leftNumber * rightNumber;
+        }
+
+        static public double div(double leftNumber, double rightNumber)
+        {
+            // check if right number can not be zero or grater then zero
+            if (rightNumber > 0)
+                return leftNumber / rightNumber;
+            throw new DivideByZeroException("Number can not be zero");
+        }
+    }
+
+}
