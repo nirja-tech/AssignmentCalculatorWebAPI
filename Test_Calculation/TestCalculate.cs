b96a8eacce9126d5ba088927202219cc67bc8115
Nirja <nirjapathak3@gmail.com>
2021-06-23 08:23:22 -0700

    Created empty test project and renamed class name.

---
+++ Test_Calculation/TestCalculate.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Calculation
+{
+    [TestClass]
+    public class TestCalculate
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
