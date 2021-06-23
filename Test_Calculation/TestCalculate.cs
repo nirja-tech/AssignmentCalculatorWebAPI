58950b2a09506dd6950d4b2c3d01221252b76bdc
Nirja <nirjapathak3@gmail.com>
2021-06-23 08:30:46 -0700

    Addede Four test Methods for Addition functionality.

--- Test_Calculation/TestCalculate.cs
+++ Test_Calculation/TestCalculate.cs
@@ -6,8 +6,49 @@ namespace Test_Calculation
     public class TestCalculate
     {
         [TestMethod]
-        public void TestMethod1()
+        [TestCategory("Addition")]
+        // This Test method checks whether adding two positive number returns a positive result or not
+        public void TestingAddingPositiveNumbersReturnPositiveResult()
         {
+            double left = 3;
+            double right = 2;
+            double result = Calculator_ClassLibrary.LibCalculator.add(left, right);
+            Assert.AreEqual(5d, result);
         }
+
+        [TestMethod]
+        [TestCategory("Addition")]
+        // This Test method check whether adding numbers having double datatypes returns double or not
+        public void TestAddingDoubleReturnsDouble()
+        {
+            double left = 3;
+            double right = 2;
+            object result = Calculator_ClassLibrary.LibCalculator.add(left, right);
+            Assert.IsInstanceOfType(result, typeof(double));
+        }
+
+        [TestMethod]
+        [TestCategory("Addition")]
+        // This Test method check whether adding two negative numbers returns negative result or not 
+        public void TestAddingNegativeNumbersReturnNegative()
+        {
+            double left = -3;
+            double right = -2;
+            double result = Calculator_ClassLibrary.LibCalculator.add(left, right);
+            Assert.IsTrue(result < 0);
+        }
+
+        [TestMethod]
+        [TestCategory("Addition")]
+        // This Test method check result should not depend upon numbers order
+        public void TestGetsSameResultEvenAfterOrderChanged()
+        {
+            double left = 3;
+            double right = 2;
+            double result = Calculator_ClassLibrary.LibCalculator.add(left, right);
+            double result1 = Calculator_ClassLibrary.LibCalculator.add(right, left);
+            Assert.IsTrue(result == result1);
+        }
+
     }
 }
